ab09b745f3248846e18473cc8de562dfa45a2efc
KrishanAchintha <152761376+KrishanAchintha@users.noreply.github.com>
2025-01-19 11:50:48 +0530

    Update ModelMapperConfig.java

--- src/main/java/com/jeewaeducation/interaction_controller/config/ModelMapperConfig.java
+++ src/main/java/com/jeewaeducation/interaction_controller/config/ModelMapperConfig.java
@@ -1,6 +1,9 @@
 package com.jeewaeducation.interaction_controller.config;
 
+import com.jeewaeducation.interaction_controller.dto.counselorSession.CounselorSessionSaveDTO;
+import com.jeewaeducation.interaction_controller.entity.CounselorSession;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,15 @@ public class ModelMapperConfig
 {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.addMappings(new PropertyMap<CounselorSessionSaveDTO, CounselorSession>() {
+            @Override
+            protected void configure() {
+                map().setCounselorId(source.getCounselorId());
+                map().setStudentId(source.getStudentId());
+                // Add other mappings as needed
+            }
+        });
+        return modelMapper;
     }
 }
