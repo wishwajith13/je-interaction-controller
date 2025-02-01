0b0c0c4600b5cdc5c85373f4d5c11885310a7ffd
KrishanAchintha <152761376+KrishanAchintha@users.noreply.github.com>
2025-02-02 04:05:10 +0530

    base auditing done

---
+++ src/main/java/com/jeewaeducation/interaction_controller/audit/AuditAwareImpl.java
@@ -0,0 +1,19 @@
+package com.jeewaeducation.interaction_controller.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * Returns the current auditor of  the application
+     * @return the current auditor
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNT_MS");
+    }
+}
---
+++ src/main/java/com/jeewaeducation/interaction_controller/entity/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.jeewaeducation.interaction_controller.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(insertable = false)
+    private String updatedBy;
+}
